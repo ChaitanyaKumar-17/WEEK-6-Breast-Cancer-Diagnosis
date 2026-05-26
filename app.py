b9d7e2a20105604c74adfe6cb87150b4c0be470e
Chaitanya Kumar <chaitanya0817@gmail.com>
2026-05-26 22:06:55 +0530

    did some changes

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ from sklearn.metrics import accuracy_score, classification_report
 cancer_data = load_breast_cancer()
 df = pd.DataFrame(cancer_data.data, columns=cancer_data.feature_names)
 df['target'] = cancer_data.target
-print(df.head())
 
 # Splitting dataDrame as features and target
 X = df.drop(columns=['target'])
@@ -21,7 +20,6 @@ corr_matrix = X.corr()
 upper_triangle = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(bool))
 columns_to_drop = [column for column in upper_triangle.columns if any(upper_triangle[column].abs() > 0.90)]
 X_reduced = X.drop(columns=columns_to_drop)
-print(X_reduced.head())
 
 # Split into training and testing sets to prevent data leakage
 X_train, X_test, y_train, y_test = train_test_split(X_reduced, y, test_size=0.2, random_state=42)
